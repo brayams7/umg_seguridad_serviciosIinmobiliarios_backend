5035efe323fafbbac22193390b8a4b1bcad7c05d
brayams <brayamsbilgs7govy@gmail.com>
2022-10-06 11:49:31 -0600

    agregando archivos de spring boot

---
+++ src/main/java/com/umg/serviciosInmobiliarios/ServiciosInmobiliariosApplication.java
@@ -0,0 +1,13 @@
+package com.umg.serviciosInmobiliarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosInmobiliariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosInmobiliariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/serviciosInmobiliarios/ServiciosInmobiliariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.serviciosInmobiliarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosInmobiliariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
